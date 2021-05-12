4adb4c26530874b4556b447c223b73935d290b60
DrTulipe0 <83580972+DrTulipe0@users.noreply.github.com>
2021-05-12 11:56:49 +0200

    Update DAO.java

--- ProjetEnchere/src/fr/formation/enchere/dal/DAO.java
+++ ProjetEnchere/src/fr/formation/enchere/dal/DAO.java
@@ -1,7 +1,20 @@
 package fr.formation.enchere.dal;
 
 public class DAO {
-	public static EnchereDAO getCourseDAO() {
+	public static EnchereDAO getEnchereDAO() {
 		return new EnchereDAOImpl();
 	}
+	public static ArticleVenduDAO getArticleDAO() {
+		return new ArticleVenduDAOImpl();
+	}
+	public static CategorieDAO getCategorieDAO() {
+		return new CategorieDAOImpl();
+	}
+	public static RetraitDAO getRetraitDAO() {
+		return new RetraitDAOImpl();
+	}
+	public static UtilisateurDAO getUtilisateurDAO() {
+		return new UtilisateurDAOImpl();
+	}
+	
 }
