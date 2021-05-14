962dc5721c8297f1e6c6f560d39456793afc4397
DrTulipe0 <83580972+DrTulipe0@users.noreply.github.com>
2021-05-14 09:43:30 +0200

    rgtr

--- ProjetEnchere/src/fr/formation/enchere/dal/EnchereDAO.java
+++ ProjetEnchere/src/fr/formation/enchere/dal/EnchereDAO.java
@@ -14,7 +14,7 @@ public interface EnchereDAO {
 	public Enchere update(Enchere enchere) throws DALException;
 	
 	//Sélectionner toutes les enchères
-	public List<Enchere> selectAll(int categorieEnchere, String libelleEnchere) throws DALException;
+	public List<Enchere> selectAll(int categorieEnchere, String libelleEnchere,  int no_utilisateur) throws DALException;
 	
 	//Supprimer une enchère
 	public Enchere delete(int no_utilisateur, int no_article) throws DALException;
